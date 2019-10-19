8756406b7a3e23dd95a0e1d3ba3dc0fda2ee66fa
Kullen Wetherby <wetherbyk71@gmail.com>
2019-10-19 11:55:32 -0400

    added items.map to calculate total"
    d

--- checkout.js
+++ checkout.js
@@ -1,12 +1,13 @@
 function calculateItemTotal(item) {
   const total = item.price * (1 - item.discount) * item.quantity
   item.total = parseInt(total * 100) / 100
-
+  
   return item
 }
 
 function calculateTotal(items) {
-  return items
+  return items.map(calculateItemTotal)
+
 }
 
 module.exports = calculateTotal
